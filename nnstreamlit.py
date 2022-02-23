a3bb948dc734e3eb3adf4b7f41466f2a788ba8d8
Saifali Patel <61909924+saifali-patel@users.noreply.github.com>
2022-02-23 17:10:34 +0400

    Update nnstreamlit.py

--- nnstreamlit.py
+++ nnstreamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+!pip install sklearn
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
